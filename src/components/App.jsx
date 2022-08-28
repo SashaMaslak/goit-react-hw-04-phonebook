6ff6bfa2f3c31db7cfc7980c56af9a9083c8acfc
SashaMaslak <owmaslak@gmail.com>
2022-08-28 22:41:53 +0300

    complet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,25 +9,30 @@ import { Filter } from 'components/Filter/Filter';
 // helpers
 import { filterContacts } from 'helpers/filterContacts';
 
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: initialContacts,
     filter: '',
   };
 
   componentDidMount() {
     const savedState = localStorage.getItem('contacts');
-    console.log(savedState);
     if (savedState) {
       this.setState({ contacts: JSON.parse(savedState) });
     }
   }
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-      const savedState = localStorage.getItem('contacts');
-      console.log(savedState);
+  componentDidUpdate(prevProps, prevState) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
     }
   }
 
